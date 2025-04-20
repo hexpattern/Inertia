2f697571d61ce62ab28c2faa67ec787a9dd8b64e
hexpattern <59071906+hexpattern@users.noreply.github.com>
2025-04-19 22:12:03 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,20 @@
+local json = {
+    ["cmd"] = "INVITE_BROWSER",
+    ["args"] = {
+        ["code"] = "33JHmGHFQe"
+    },
+    ["nonce"] = 'a'
+}
+spawn(function()
+    print(request({
+        Url = 'http://127.0.0.1:6463/rpc?v=1',
+        Method = 'POST',
+        Headers = {
+            ['Content-Type'] = 'application/json',
+            ['Origin'] = 'https://discord.com'
+        },
+        Body = game:GetService('HttpService'):JSONEncode(json),
+    }).Body)
+end)
+
+game.Players.LocalPlayer:Kick("\nScript Moved\n\nPlease Join The Discord\nFor Script Access:/nhttps://discord.gg/33JHmGHFQe")
